cbb751abeaefa2703bc6852dcc4e75559401389f
Alexandr Tolstov <SESA574650@se.com>
2020-11-12 20:40:36 +0600

    Add project files.

---
+++ CarEvents/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        // Is the car alive or dead?
+        private bool carIsDead;
+        // Class constructors.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // 1) Define a delegate type.
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> AboutToBlow;
+
+        // 4) Implement the Accelerate() method to invoke the delegate's
+        // invocation list under the correct circumstances.
+        public void Accelerate(int delta)
+        {
+            // If this car is "dead," send dead message.
+            if (carIsDead)
+            {
+                Exploded?.Invoke(this, new CarEventArgs("Sorry, this car is dead..."));
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // Is this car "almost dead"?
+                if (10 == (MaxSpeed - CurrentSpeed))
+                {
+                    AboutToBlow?.Invoke(this, new CarEventArgs("Sorry, this car is dead..."));
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
